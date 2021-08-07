94fa1c225d58d8156d0682a7aa625eefeaf85890
Alexander Koskovich <zvnexus@outlook.com>
2021-08-07 18:18:24 +0200

    sm8250-common: Import lineage framework matrix
    * from https://github.com/LineageOS/android_vendor_lineage/commit/78870c267fa57b3cb98607c0715504e6821fd7b6
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -128,7 +128,7 @@ TARGET_SURFACEFLINGER_FOD_LIB := //$(COMMON_PATH):libfod_extension.oneplus_kona
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(COMMON_PATH)/lineage_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
